f12f30358468639a42e1b9afdcbc27cf82cfc74c
jntakpe <j.ntakpe@gmail.com>
2014-09-19 22:55:06 +0200

    Ajout de Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,21 @@
+/**
+ * Fichier de configuration de Grunt permettant de builder les ressources 'statiques'
+ *
+ * @param grunt
+ * @author jntakpe
+ */
+module.exports = function (grunt) {
+    'use strict';
+    grunt.util.linefeed = '\n';
+
+    grunt.initConfig();
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+};
