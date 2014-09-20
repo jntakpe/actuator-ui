180d983b8675e5cbe8c1082be98c314a6bdf9cba
jntakpe <j.ntakpe@gmail.com>
2014-09-20 11:24:52 +0200

    Ajout de la gestion des aspects au niveau des controleurs

---
+++ src/main/java/com/github/jntakpe/web/ControllerAdviser.java
@@ -0,0 +1,29 @@
+package com.github.jntakpe.web;
+
+import com.github.jntakpe.util.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+/**
+ * Aspects transversaux aux contrôleurs
+ *
+ * @author jntakpe
+ */
+@ControllerAdvice
+public class ControllerAdviser {
+
+    @Autowired
+    private Environment environment;
+
+    /**
+     * Indique aux vues si l'application est sur un profil de développement
+     *
+     * @return true si mode de développement
+     */
+    @ModelAttribute("isDevMode")
+    public boolean isDevMode() {
+        return Profile.valueOf(environment.getActiveProfiles()[0].toUpperCase()).isDevProfile();
+    }
+}
